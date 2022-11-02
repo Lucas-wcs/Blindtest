981e51b9e8351b735e55316d81428218ce6f925d
Lucas-wcs <112962818+Lucas-wcs@users.noreply.github.com>
2022-11-02 10:32:16 +0100

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
-const router = require("./router");
+const musicRoutes = require("./router");
 
 const app = express();
 
