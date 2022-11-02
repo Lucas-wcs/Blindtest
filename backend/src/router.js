a2e1953338f2f6171d3dd49a9b59bac1d4062b7c
Baptiste Courtin <bap2910@gmail.com>
2022-11-02 10:38:33 +0100

    back

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,16 +1,12 @@
-const router = require('express').Router();
-const datas = require('../datas');
-
-
+const router = require("express").Router();
+const datas = require("../datas");
 
 router.get("/", (req, res) => {
-    res.json(datas).status(200);
+  res.json(datas).status(200);
 });
 
 router.get("/:filename", (req, res) => {
-    res.sendFile(`./public/mp3/${req.params.filename}`, {root : './'})
+  res.sendFile(`./public/mp3/${req.params.filename}`, { root: "./" });
 });
-
-
 
 module.exports = router;
