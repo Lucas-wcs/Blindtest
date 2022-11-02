1b1ed7b92c9ff588aa312c7d5af3c7d563fb5230
JeremieLarousse <97170000+JeremieLarousse@users.noreply.github.com>
2022-11-02 14:51:47 +0100

    push before back end

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -3,34 +3,95 @@ import MyTitle from "../components/user/MyTitle";
 import LastListen from "../components/user/LastListen";
 import Favoris from "../components/user/Favoris";
 import "../style/User.css";
+import "../App.css";
 import "boxicons";
 
 function User() {
   const lastEcoute = [
     {
+      img: (
+        <img
+          className="imgMusic"
+          src="https://www.noviscore.fr/pochette255-eye-of-the-tiger-survivor.jpg"
+          alt="img"
+        />
+      ),
       artist: "Survivor",
       title: "Eye of the tiger",
-      pieceToListen: "test",
+      pieceToListen: "Favoris",
+      btn: React.createElement(
+        "button",
+        { type: "button" },
+        <box-icon color="orange" name="caret-right-circle" />
+      ),
     },
     {
+      img: (
+        <img
+          className="imgMusic"
+          src="https://www.noviscore.fr/pochette255-eye-of-the-tiger-survivor.jpg"
+          alt="img"
+        />
+      ),
       artist: "Survivor",
       title: "Eye of the tiger",
-      pieceToListen: "test",
+      pieceToListen: "Favoris",
+      btn: React.createElement(
+        "button",
+        { type: "button" },
+        <box-icon color="orange" name="caret-right-circle" />
+      ),
     },
     {
+      img: (
+        <img
+          className="imgMusic"
+          src="https://www.noviscore.fr/pochette255-eye-of-the-tiger-survivor.jpg"
+          alt="img"
+        />
+      ),
       artist: "Survivor",
       title: "Eye of the tiger",
-      pieceToListen: "test",
+      pieceToListen: "Favoris",
+      btn: React.createElement(
+        "button",
+        { type: "button" },
+        <box-icon color="orange" name="caret-right-circle" />
+      ),
     },
     {
+      img: (
+        <img
+          className="imgMusic"
+          src="https://www.noviscore.fr/pochette255-eye-of-the-tiger-survivor.jpg"
+          alt="img"
+        />
+      ),
       artist: "Survivor",
       title: "Eye of the tiger",
-      pieceToListen: "test",
+      pieceToListen: "Favoris",
+      btn: React.createElement(
+        "button",
+        { type: "button" },
+        <box-icon color="orange" name="caret-right-circle" />
+      ),
     },
     {
+      img: (
+        <img
+          className="imgMusic"
+          src="https://www.noviscore.fr/pochette255-eye-of-the-tiger-survivor.jpg"
+          alt="img"
+        />
+      ),
       artist: "Survivor",
       title: "Eye of the tiger",
-      pieceToListen: "test",
+      pieceToListen: "Favoris",
+      btn: React.createElement(
+        "button",
+        { type: "button" },
+        <box-icon color="orange" name="caret-right-circle" />
+      ),
     },
   ];
 
@@ -46,7 +107,7 @@ function User() {
       titre: "Eye of the tiger",
       artiste: "Survivor",
       duree: "3:00",
-      favoris: true,
+      favoris: "Favoris",
       btn: React.createElement(
         "button",
         { type: "button" },
@@ -64,7 +125,7 @@ function User() {
       titre: "Eye of the tiger",
       artiste: "Survivor",
       duree: "3:00",
-      favoris: true,
+      favoris: "Favoris",
       btn: React.createElement(
         "button",
         { type: "button" },
@@ -82,7 +143,7 @@ function User() {
       titre: "Eye of the tiger",
       artiste: "Survivor",
       duree: "3:00",
-      favoris: true,
+      favoris: "Favoris",
       btn: React.createElement(
         "button",
         { type: "button" },
@@ -100,7 +161,7 @@ function User() {
       titre: "Eye of the tiger",
       artiste: "Survivor",
       duree: "3:00",
-      favoris: true,
+      favoris: "Favoris",
       btn: React.createElement(
         "button",
         { type: "button" },
@@ -118,7 +179,61 @@ function User() {
       titre: "Eye of the tiger",
       artiste: "Survivor",
       duree: "3:00",
-      favoris: true,
+      favoris: "Favoris",
+      btn: React.createElement(
+        "button",
+        { type: "button" },
+        <box-icon color="orange" name="caret-right-circle" />
+      ),
+    },
+    {
+      img: (
+        <img
+          className="imgMusic"
+          src="https://www.noviscore.fr/pochette255-eye-of-the-tiger-survivor.jpg"
+          alt="img"
+        />
+      ),
+      titre: "Eye of the tiger",
+      artiste: "Survivor",
+      duree: "3:00",
+      favoris: "Favoris",
+      btn: React.createElement(
+        "button",
+        { type: "button" },
+        <box-icon color="orange" name="caret-right-circle" />
+      ),
+    },
+    {
+      img: (
+        <img
+          className="imgMusic"
+          src="https://www.noviscore.fr/pochette255-eye-of-the-tiger-survivor.jpg"
+          alt="img"
+        />
+      ),
+      titre: "Eye of the tiger",
+      artiste: "Survivor",
+      duree: "3:00",
+      favoris: "Favoris",
+      btn: React.createElement(
+        "button",
+        { type: "button" },
+        <box-icon color="orange" name="caret-right-circle" />
+      ),
+    },
+    {
+      img: (
+        <img
+          className="imgMusic"
+          src="https://www.noviscore.fr/pochette255-eye-of-the-tiger-survivor.jpg"
+          alt="img"
+        />
+      ),
+      titre: "Eye of the tiger",
+      artiste: "Survivor",
+      duree: "3:00",
+      favoris: "Favoris",
       btn: React.createElement(
         "button",
         { type: "button" },
@@ -132,17 +247,20 @@ function User() {
       <header>
         <p>HEADER</p>
       </header>
+      <div className="blockMusic">
+        <div className="titleAndLast">
+          <div className="titleUser">
+            <MyTitle />
+          </div>
 
-      <div className="titleUser">
-        <MyTitle />
-      </div>
+          <div className="lastListen">
+            <LastListen lastEcoute={lastEcoute} />
+          </div>
+        </div>
 
-      <div className="lastListen">
-        <LastListen lastEcoute={lastEcoute} />
-      </div>
-
-      <div className="pieceOfMusic">
-        <Favoris fav={fav} />
+        <div className="pieceOfMusic">
+          <Favoris fav={fav} />
+        </div>
       </div>
     </div>
   );
