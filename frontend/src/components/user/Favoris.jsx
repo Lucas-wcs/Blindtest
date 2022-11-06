c677a1e86f1bba3c3a38ee14f88623b920ee6278
JeremieLarousse <97170000+JeremieLarousse@users.noreply.github.com>
2022-11-06 17:17:14 +0100

    third commit

--- frontend/src/components/user/Favoris.jsx
+++ frontend/src/components/user/Favoris.jsx
@@ -12,13 +12,13 @@ function Favoris({ fav }) {
       <div className="aBlock">
         {fav.map((music) => {
           return (
-            <div>
-              <img src={music.pochette} alt="pochette" />
-              <p>
-                {music.titre} {music.artiste}
+            <div className="musicDivBlock">
+              <img className="imgMusic" src={music.pochette} alt="pochette" />
+              <p className="titleAndArtist">
+                {music.titre} / {music.artiste}
               </p>
               <Player
-                style={{ borderRadius: "1rem" }}
+                className="MusicPlayerBox"
                 src={`http://localhost:5000/mp3/${music.mp3}`}
                 showJumpControls={false}
               />
