e969b2f0bb8b1a0ee82910e490e580271093e8be
JeremieLarousse <97170000+JeremieLarousse@users.noreply.github.com>
2022-11-16 09:41:27 +0100

    push before fav

--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -1,7 +1,3 @@
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import React, { useState } from "react";
-// import Dashboard from "../components/user/Dashboard/Dashboard";
-// import Preferences from "../components/user/Preferences/Preferences";
 import Login from "../components/user/Login/Login";
 import useToken from "../components/user/useToken";
 import User from "./User";
@@ -15,16 +11,6 @@ function Connexion() {
   return (
     <div className="wrapper">
       <User />
-      {/* <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
-      </BrowserRouter> */}
     </div>
   );
 }
